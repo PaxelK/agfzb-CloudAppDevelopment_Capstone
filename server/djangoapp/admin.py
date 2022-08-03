4d4ced8935c9503162f53b8da67f42251812250c
PaxelK <axels_ibm1@hotmail.com>
2022-08-03 21:55:15 +0200

    new Update

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,33 @@
 from django.contrib import admin
 # from .models import related models
+from .models import Car, CarMake
 
 
 # Register your models here.
 
 # CarModelInline class
+"""
+    carMake = models.ForeignKey(CarMake,on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    dealer_id = models.IntegerField()
+    car_type = models.CharField(max_length=30)
+    year = models.IntegerField()
+"""
+class CarInline(admin.StackedInline):
+    model = Car
+    list_display = ['name', 'dealer_id', 'car_type', 'year']
 
 # CarModelAdmin class
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['name', 'dealer_id', 'car_type', 'year']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarInline]
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
 
 # Register models here
+admin.site.register(Car, CarAdmin)
+admin.site.register(CarMake,CarMakeAdmin)
